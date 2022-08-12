875dabb21efafcaac033c2f0aea24519683e614e
Riley Flynn <riley@rileyflynn.me>
2022-08-11 22:00:37 -0230

    Add basic script to list images

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { Client } from "@notionhq/client";
+import { Client,  } from "@notionhq/client";
+import { PageObjectResponse, PartialPageObjectResponse, TitlePropertyItemObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -8,11 +9,52 @@ async function main() {
     auth: process.env.NOTION_TOKEN,
   });
 
-  const response = await notion.databases.query({
-    database_id: "FIXME",
-  });
+  const stickerPages: (PageObjectResponse | PartialPageObjectResponse)[] = [];
 
-  console.log("Got response:", response);
+  let response = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID!,
+  });
+  while (response.results.length > 0) {
+    stickerPages.push(...response.results);
+    if (response.has_more) {
+      response = await notion.databases.query({
+        database_id: process.env.NOTION_DATABASE_ID!,
+        start_cursor: response.next_cursor!,
+      });
+    } else {
+      break;
+    }
+  }
+
+  console.log(stickerPages.length);
+  
+  for (const dbPage of stickerPages) {
+    const titleResponse = await notion.pages.properties.retrieve({
+      page_id: dbPage.id,
+      property_id: "title",
+    });
+    if (titleResponse.type !== "property_item") {
+      throw new Error("title is not a property item");
+    }
+    const titleObject = titleResponse.results[0];
+    if (titleObject.type !== "title") {
+      throw new Error("title is not a title");
+    }
+
+    const blocks = await notion.blocks.children.list({
+      block_id: dbPage.id,
+    });
+    const imageBlock = blocks.results[0];
+    if (!("type" in imageBlock)) {
+      throw new Error("first block is a partial");
+    }
+    if (imageBlock.type !== "image") {
+      console.log(titleObject.title.plain_text);
+      throw new Error("first block is not an image");
+    }
+
+    const image = imageBlock.image;
+  }
 }
 
 main()
